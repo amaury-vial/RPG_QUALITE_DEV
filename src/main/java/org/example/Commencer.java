0b40abc0f58c58cd0aea96b0b105215c074ab795
Djibril <djibril.ramdani@etu.univ-amu.fr>
2022-12-11 21:30:55 +0100

    Amélioration classe "Commencer" Erreur pour le sexe

--- src/main/java/org/example/Commencer.java
+++ src/main/java/org/example/Commencer.java
@@ -14,25 +14,45 @@ public class Commencer {
         String nom = sc.nextLine();
         System.out.println("Très bien " + nom + ". C'est un très beau nom . Quel est votre age ?");
         int age = sc.nextInt();
-        if (age < 12)
+        while (age < 13)
         {
-            System.out.println("Tu n'as pas l'âge nécéssaire pour jouer a ce jeu mais je vais faire une exception pour cette fois");
+                System.out.println("Tu es trop jeune pour ce jeu");
+                 age = sc.nextInt();
+
         }
-        System.out.println("De quel sexe es-tu ?");
+        System.out.println("De quel sexe es-tu ? H pour Homme et F pour Femme");
         Scanner sc1 = new Scanner(System.in);
 
         String sexe = sc1.nextLine();
+        do
+        {
+            System.out.println("Soit H soit F !");
+            sexe = sc1.nextLine();
+        }
+            while (sexe != "H"  || sexe !="F");
+
         System.out.println("On est presque bon. Quelle est ta taille ? (en cm)");
         int taille = sc.nextInt();
-        System.out.println("Pour finir, quelle classe veux tu choisir ? Guerrier , Magicien , Rodeur , Voleur");
+        if (taille < 130)
+            System.out.println("Ok le nain");
+        else if (taille > 190)
+            System.out.println("Tu as déja songé au basket ?");
+        System.out.println("Pour finir, quelle classe veux tu choisir ? G pour Guerrier , M pour Magicien , R pour Rodeur et V pour  Voleur");
         String classe = sc.nextLine();
+        while (classe !="G" || classe !="M" || classe !="R" || classe!="V" )
+        {
+            System.out.println("Veuillez choisir une classe proposée!");
+            classe = sc.nextLine();
+        }
         System.out.println("Ok tout est bon pour nous");
         System.out.println("Place à l'attribution des points");
+        System.out.println("Lorsque tu choisiras ou mettre tes points , 5 points seront attribués");
 
         Joueur test = new Joueur(nom , sexe , age , taille , classe);
 
         while (test.pointsRestantsAAtribuer()) {
             System.out.println("Ou mettre tes points ? 1 pour agilité  , 2 pour chance , 3 pour force");
+            System.out.println("Il te reste 0 " +test.pointsRestantsAAtribuer());
             int statChoisi = sc.nextInt();
 
 
